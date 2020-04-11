aaa7fe18962523468fd06608f019dc59f37cf3a3
chikwado17 <chikwadonworie@gmail.com>
2020-04-11 14:56:31 +0100

    Updates

---
+++ precache-manifest.ff2d5086ef3abd077e1065bcdc9418dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c090d1a9ac66cde4ea1ac50418355e",
+    "url": "/moster/index.html"
+  },
+  {
+    "revision": "5d4453ed94d1486d757f",
+    "url": "/moster/static/css/main.40241373.chunk.css"
+  },
+  {
+    "revision": "7638948e165a63c6306c",
+    "url": "/moster/static/js/2.c6b1dc3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moster/static/js/2.c6b1dc3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d4453ed94d1486d757f",
+    "url": "/moster/static/js/main.66948a0d.chunk.js"
+  },
+  {
+    "revision": "33730b48f9a40040221a",
+    "url": "/moster/static/js/runtime-main.9bbcaedb.js"
+  }
+]);
